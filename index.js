7e0a3f86581bc0e0e4138d7253855d0101573d9e
Sneha Trivedi <snehatrivedi1009@gmail.com>
2023-03-10 23:08:29 +0530

    Chore: create some modifications in routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const routes = require("./src/routers/routers");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 // app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.listen(port, () =>
